5dee41347b666899f7ceefd544c632c478b0aa04
Prajwal Gautam <prajwalgautam08@gmail.com>
2024-01-28 21:37:06 +0545

    Feat: Banner Section done.

--- src/features/home/components/BannerSection.tsx
+++ src/features/home/components/BannerSection.tsx
@@ -1,14 +1,19 @@
-import React from "react";
-import { Box, Center, Stack } from "@chakra-ui/react";
+import { Box, Center, Image, Stack } from "@chakra-ui/react";
+import SectionTitle from "./SectionTitle";
+
+import visitNepal from "../../../assets/images/visit_nepal.jpg";
 
 const BannerSection = () => {
   return (
-    <Stack>
+    <Stack spacing={2}>
+      <Image objectFit="cover" src={visitNepal} width="full" height={"70vh"} />
       <Box>
-        <Center fontSize="5xl" className="dancing-font">
-          Official Website of
+        <Center>
+          <SectionTitle title="Official Website of" />
         </Center>
-        <Center fontSize="5xl" className="dancing-font">Travel.com</Center>
+        <Center>
+          <SectionTitle title="Travel.com" />
+        </Center>
       </Box>
     </Stack>
   );
