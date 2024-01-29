54ddeb3cec924eb8a30d6388119f0013c22cde83
Prajwal Gautam <prajwalgautam08@gmail.com>
2024-01-29 15:04:00 +0545

    Feat: Admin Login page

--- src/features/admin/routes/AdminRoutes.tsx
+++ src/features/admin/routes/AdminRoutes.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import AdminLoginPage from "../pages/AdminLoginPage";
+import AdminHomePage from "../pages/AdminHomePage";
 
 const AdminRoutes = () => {
   return (
     <Routes>
       <Route path="login" element={<AdminLoginPage />} />
+      <Route path="/" element={<AdminHomePage />} />
     </Routes>
   );
 };
