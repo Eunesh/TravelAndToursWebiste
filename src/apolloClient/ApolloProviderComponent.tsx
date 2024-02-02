11b7e8e1cbdac11cb4f0f78a403c2d7188053598
Prajwal Gautam <prajwalgautam08@gmail.com>
2024-02-02 15:40:49 +0545

    Fix: fetched graphql uri from .env file

--- src/apolloClient/ApolloProviderComponent.tsx
+++ src/apolloClient/ApolloProviderComponent.tsx
@@ -1,10 +1,14 @@
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { FC } from "react";
 
 const client = new ApolloClient({
-  uri: "http://localhost:8000/graphql", // Graphql API endpoint
+  uri: import.meta.env.VITE_API_GRAPHQL_URL, // Graphql API endpoint
   cache: new InMemoryCache(),
 });
 
-export const ApolloComponent = ({ children }: any) => {
+interface IApolloComponent {
+  children: React.ReactNode | Array<React.ReactNode> | undefined;
+}
+export const ApolloComponent: FC<IApolloComponent> = ({ children }) => {
   return <ApolloProvider client={client}>{children}</ApolloProvider>;
 };
